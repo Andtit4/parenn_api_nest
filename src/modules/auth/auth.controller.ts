6894c4215d1410c8c2f8a1adf9624f42796939fa
Andtit4 <lirsitogo2021@gmail.com>
2025-08-06 15:19:35 +0000

    add get current connected by token

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -5,11 +5,13 @@ import {
   Post,
   UnauthorizedException,
   Request,
+  UseGuards,
 } from '@nestjs/common';
 import { UserRepositoryImpl } from './infrastructure/typeorm/repositories/user.repository.impl';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { RegisterUseCase } from './application/use-case/register.use-case';
+import { JwtAuthGuard } from 'src/shared/guards/jwt-guard';
 
 @Controller('auth')
 export class AuthController {
@@ -22,6 +24,8 @@ export class AuthController {
   async login(@Body() body) {
     const user = await this.userRepo.findByEmail(body.email);
     if (!user) throw new UnauthorizedException('Invalid credentials');
+
+    // const password_hashed = await bcrypt.hashed(body.password, 10)
 
     const isMatch = await bcrypt.compare(body.password, user.password);
     if (!isMatch) throw new UnauthorizedException('Invalid credentials');
@@ -39,10 +43,13 @@ export class AuthController {
 
   @Post('register')
   async register(@Body() body) {
+    const emailAlreadyUsed = await this.userRepo.findByEmail(body.email);
+    if (emailAlreadyUsed) throw new UnauthorizedException('Email already used');
     const useCase = new RegisterUseCase(this.userRepo);
     return useCase.execute(body.email, body.username, body.password, 'CLIENT');
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('me')
   async getMe(@Request() req) {
     return req.user;
